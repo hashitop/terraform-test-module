aaaaaee8a0815fba55a2167ef832d9fb85616163
Top <50546939+hashitop@users.noreply.github.com>
2020-09-11 11:14:09 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "aws_vpc" "terraform-test-provider-vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
 
-  tags = {
-    "Environment" = "ptfe"
+resource "null_resource" {
   }
-}
