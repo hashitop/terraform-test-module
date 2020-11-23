ceeceb0cc5f03435af92f52c78eb8279583d1b6b
tmekaumput <kampanart_m@yahoo.com>
2020-11-23 17:18:47 +1100

    fix

--- sub2/main.tf
+++ sub2/main.tf
@@ -5,5 +5,5 @@ resource "null_resource" "test" {
 }
 
 module "sub2" {
-  source = "localterraform.com/kampanart-org/module/test//sub1/sub2"
+  source = "tfe-apac-test1.hashicorp-success.com/kampanart-org/module/test//sub1/sub2"
 }
